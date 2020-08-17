1b934962ec3c4d49161d4ea192dcafedb96912db
Matthew Broberg <matt@opensource.com>
2020-08-17 15:10:29 -0500

    Fix in vscode script
    - `uniq` requires sorted lists to dedup. Shwoops.

--- run_once_vscode-plugins.fish
+++ run_once_vscode-plugins.fish
@@ -4,7 +4,7 @@
 code --list-extensions > /tmp/vscodelistextensions
 
 # append to chezmoi saved state of extensions
-cat /tmp/vscodelistextensions ~/.vscode/extensions-list | uniq -u > ~/.vscode/extensions-list
+sort ~/.vscode/extensions-list /tmp/vscodelistextensions | uniq > ~/.vscode/extensions-list
 
 # install them 
 cat ~/.vscode/extensions-list | xargs -n1 code --install-extension
