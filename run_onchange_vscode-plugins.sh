0d25785b00ca81696a68d109c338627628235510
Matthew Broberg <matt@opensource.com>
2024-03-16 13:51:12 -0500

    Obsidian config + templating

--- run_onchange_vscode-plugins.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh 
-
-# How to actually append installed plugins into a list of plugins 
-code --list-extensions >> ~/.vscode/extensions-list
-# then install 
-uniq ~/.vscode/extensions-list | xargs -n1 code --install-extension
