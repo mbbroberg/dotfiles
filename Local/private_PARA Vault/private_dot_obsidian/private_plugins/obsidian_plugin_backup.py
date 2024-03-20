ffd5227fcf788eb660d0d3a4f3cd3d8ecde1d3ad
Matthew Broberg <matt@opensource.com>
2024-03-19 21:54:54 -0500

    Add Local/PARA Vault/.obsidian/plugins/obsidian_plugin_backup.py Add obsidian/callout-manager/data.json Update obsidian/card-board/data.json Update obsidian/card-board/manifest.json Add obsidian/cmdr/data.json Add obsidian/dataview/data.json Add obsidian/file-tree-alternative/data.json Add obsidian/nldates-obsidian/data.json Add obsidian/obsidian-day-planner/data.json Add obsidian/obsidian-minimal-settings/data.json Add obsidian/obsidian-omnivore/data.json Add obsidian/obsidian-statusbar-pomo-fork/data.json Add obsidian/obsidian-tasks-plugin/data.json Add obsidian/periodic-notes/data.json Add obsidian/templater-obsidian/data.json

---
+++ Local/private_PARA Vault/private_dot_obsidian/private_plugins/obsidian_plugin_backup.py
@@ -0,0 +1,34 @@
+import os
+
+source_dir = os.path.expanduser("/Users/mbbroberg/Local/PARA Vault/.obsidian/plugins/")
+home_dir = os.path.expanduser("~/obsidian/")
+
+def check_hard_link(src_path, dest_path):
+    try:
+        src_inode = os.stat(src_path).st_ino
+        dest_inode = os.stat(dest_path).st_ino
+        return src_inode == dest_inode
+    except OSError as e:
+        print(f"Error checking hard link: {e}")
+        return False
+
+def create_hard_links(directory):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file == "data.json":
+                # print(f"Found data.json in {root}")
+                src_path = os.path.join(root, file)
+                relative_path = os.path.relpath(root, directory)
+                dest_dir = os.path.join(home_dir, relative_path)
+                try:
+                    os.makedirs(dest_dir, exist_ok=True)
+                    dest_path = os.path.join(dest_dir, file)
+                    os.link(src_path, dest_path)
+                    if check_hard_link(src_path, dest_path):
+                        print(f"Hard link created successfully: {dest_path}")
+                    else:
+                        print(f"Failed to create hard link: {dest_path}")
+                except OSError as e:
+                    print(f"Error creating hard link: {e}")
+
+create_hard_links(source_dir)
